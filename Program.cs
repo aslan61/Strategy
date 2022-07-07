9661a05d983134a825b413ea851e8d1bb175fe87
Ahmet Aslan <simsar6155@gmail.com>
2022-07-07 17:50:58 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.crediCalculatorBase = new Before210CreditCalculator();
+            customerManager.SaveCredit();
+
+            customerManager.crediCalculatorBase = new After210CreditCalculator();
+            customerManager.SaveCredit();
+            Console.ReadLine();
+
+        }
+    }
+    abstract class CrediCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+    class Before210CreditCalculator : CrediCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using before210");
+        }
+    }
+    class After210CreditCalculator : CrediCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using after210");
+        }
+    }
+    class CustomerManager
+    {
+        public CrediCalculatorBase crediCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager business");
+            crediCalculatorBase.Calculate();
+        }
+    }
+
+}
